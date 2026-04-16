00d6377b0d68f93c41a2183325d7a7e1b02ac8db
carlos oporta <elcalus456@gmail.com>
2026-04-16 10:05:21 -0600

    nombre

--- src/components/navegacion/Encabezado.jsx
+++ src/components/navegacion/Encabezado.jsx
@@ -101,7 +101,7 @@ const Encabezado = () => {
             className="d-inline-block me-2"
           />
           <strong>
-            <h4 className="mb-0">Discosa</h4>
+            <h4 className="mb-0">RevPartsPro</h4>
           </strong>
         </Navbar.Brand>
 
