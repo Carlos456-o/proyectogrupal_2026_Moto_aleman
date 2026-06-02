4efa115d8f25e2518a30f78606e065acfbd10a04
carlos oporta <elcalus456@gmail.com>
2026-06-02 09:20:41 -0600

    coreccion de texto

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -14,7 +14,7 @@ const Inicio = () => {
 
   return (
     <div className="inicio-container">
-      {/* SECCI�N HERO */}
+      {/* SECCION HERO */}
       <section className="hero-section">
         <Container fluid className="hero-content">
           <Row className="align-items-center h-100">
@@ -22,22 +22,19 @@ const Inicio = () => {
               <h1 className="hero-title">
                 BIEN<span className="text-oro">VENIDOS</span>
               </h1>
-              <h2 className="hero-subtitle">MOTO REPUESTOS ALEM�N</h2>
-              <p className="hero-tagline">"Tu mejor opci�n en repuestos de motos"</p>
+              <h2 className="hero-subtitle">MOTO REPUESTOS ALEMAN</h2>
+              <p className="hero-tagline">"Tu mejor opcion en repuestos de motos"</p>
               <p className="hero-description">
-                En Moto Repuestos Alem�n, nos apasiona el mundo de las motos y nos 
-                dedicamos a ofrecerte los mejores repuestos para tu veh�culo. Con una 
+                En Moto Repuestos Aleman, nos apasiona el mundo de las motos y nos 
+                dedicamos a ofrecerte los mejores repuestos para tu vehiculo. Con una 
                 amplia gama de productos de alta calidad. Ya sea que necesites piezas 
-                de motor, frenos, suspensi�n o accesorios.
+                de motor, frenos, suspension o accesorios.
                 <br />
                 <br />
-                �Explora nuestro cat�logo y encuentra los repuestos perfectos para tu 
-                moto hoy mismo! �Desc�brenos!
+               Explora nuestro catalogo y encuentra los repuestos perfectos para tu 
+                moto hoy mismo!Descbrenos!
               </p>
-              <Button className="btn-leer-mas">
-                LEER MAS 
-                <i className="bi bi-arrow-right ms-2"></i>
-              </Button>
+
             </Col>
             <Col lg={6} className="hero-visual">
               <div className="hero-wrapper">
